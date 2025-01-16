9f7be03df64ba6a3d5a1fd359eda932b45cb0bfb
nednoodlehead <102321690+nednoodlehead@users.noreply.github.com>
2025-01-16 09:08:16 -0800

    Add files via upload
    helix config and personal startup script :)

---
+++ personal_startup.ps1
@@ -0,0 +1,4 @@
+﻿Start-Process -FilePath "C:\Program Files\Mozilla Firefox\firefox.exe" -WindowStyle Maximized
+Start-Process -FilePath "C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE"
+Start-Process -FilePath "C:\Program Files\WindowsApps\Microsoft.WindowsTerminal_1.21.3231.0_x64__8wekyb3d8bbwe\WindowsTerminal.exe" -ArgumentList "--maximized", "-d", "C:\Users\$Env:UserName\Documents\projects\punge", ";", "new-tab", "-d", "C:\Users\$Env:UserName\Documents\projects\punge", "focus-tab", "-t", "0" -WindowStyle Maximized
+exit
