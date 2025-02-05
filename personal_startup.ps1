48b8c2c02c7172fe9e39febc7f78f4b758429c42
nednoodlehead <102321690+nednoodlehead@users.noreply.github.com>
2025-02-05 09:15:05 -0800

    Update personal_startup.ps1
    fixed terminal versioning issue..

--- personal_startup.ps1
+++ personal_startup.ps1
@@ -1,4 +1,5 @@
-﻿Start-Process -FilePath "C:\Program Files\Mozilla Firefox\firefox.exe" -WindowStyle Maximized
+$term_name = Get-ChildItem "C:\Program Files\WindowsApps" | Where-Object {$_.Name -like "Microsoft.WindowsTerminal*"} | Select-Object -First 1
+$term_path = "C:\Program Files\WindowsApps\" + $term_name + "\WindowsTerminal.exe"
+Start-Process -FilePath "C:\Program Files\Mozilla Firefox\firefox.exe" -WindowStyle Maximized
 Start-Process -FilePath "C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE"
-Start-Process -FilePath "C:\Program Files\WindowsApps\Microsoft.WindowsTerminal_1.21.3231.0_x64__8wekyb3d8bbwe\WindowsTerminal.exe" -ArgumentList "--maximized", "-d", "C:\Users\$Env:UserName\Documents\projects\punge", ";", "new-tab", "-d", "C:\Users\$Env:UserName\Documents\projects\punge", "focus-tab", "-t", "0" -WindowStyle Maximized
-exit
+Start-Process -FilePath $term_path -ArgumentList "--maximized", "-d", "C:\Users\$env:username\Documents\projects\punge", ";", "new-tab", "-d", "C:\Users\$env:username\Documents\projects\punge", "focus-tab", "-t", "0"
